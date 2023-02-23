f39984ba6e6d3ee18c24d9983fc08907dfd16043
RogeMB <mohigefer.barog22@triana.salesianos.edu>
2023-02-23 08:45:42 +0100

    avances en el login

--- solo_bajos/lib/main.dart
+++ solo_bajos/lib/main.dart
@@ -5,36 +5,28 @@ import 'package:solo_bajos/blocs/blocs.dart';
 import 'package:solo_bajos/services/services.dart';
 import 'package:solo_bajos/pages/pages.dart';
 
-
-
 void main() {
   //WidgetsFlutterBinding.ensureInitialized();
   //await SharedPreferences.getInstance();
   setupAsyncDependencies();
   configureDependencies();
   //await getIt.allReady();
-  
-    
-    runApp(BlocProvider<AuthenticationBloc>(
-        create: (context) {
-          //GlobalContext.ctx = context;
-          final authService = getIt<JwtAuthenticationService>();
-          return AuthenticationBloc(authService)..add(AppLoaded());
-        },
-        child: MyApp(),
-      ));
 
+  runApp(BlocProvider<AuthenticationBloc>(
+    create: (context) {
+      //GlobalContext.ctx = context;
+      final authService = getIt<JwtAuthenticationService>();
+      return AuthenticationBloc(authService)..add(AppLoaded());
+    },
+    child: MyApp(),
+  ));
 }
 
 class GlobalContext {
-  
   static late BuildContext ctx;
-
 }
 
-
 class MyApp extends StatelessWidget {
-
   //static late  AuthenticationBloc _authBloc;
 
   static late MyApp _instance;
@@ -63,9 +55,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //GlobalContext.ctx = context;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Authentication Demo',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.brown,
       ),
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: (context, state) {
--- solo_bajos/lib/pages/home_page.dart
+++ solo_bajos/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solo_bajos/blocs/authentication/authentication.dart';
@@ -15,7 +16,7 @@ class HomePage extends StatelessWidget {
     final authBloc = BlocProvider.of<AuthenticationBloc>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: const Text('Home Page'),
       ),
       body: SafeArea(
         minimum: const EdgeInsets.all(16),
@@ -24,30 +25,30 @@ class HomePage extends StatelessWidget {
             children: <Widget>[
               Text(
                 'Welcome, ${user.fullName}',
-                style: TextStyle(
-                  fontSize: 24
-                ),
+                style: const TextStyle(fontSize: 24),
               ),
               const SizedBox(
                 height: 12,
               ),
               ElevatedButton(
-                //textColor: Theme.of(context).primaryColor,
-                /*style: TextButton.styleFrom(
-                  primary: Theme.of(context).primaryColor,
-                ),*/
-                child: Text('Logout'),
-                onPressed: (){
+                style: TextButton.styleFrom(
+                  foregroundColor: Theme.of(context).primaryColor,
+                ),
+                child: const Text('Logout'),
+                onPressed: () {
                   authBloc.add(UserLoggedOut());
                 },
               ),
-              ElevatedButton(onPressed: () async {
-                print("Check");
-                JwtAuthenticationService service = getIt<JwtAuthenticationService>();
-                await service.getCurrentUser();
-              }
-              , child: Text('Check')
-              )
+              ElevatedButton(
+                  onPressed: () async {
+                    if (kDebugMode) {
+                      print("Check");
+                    }
+                    JwtAuthenticationService service =
+                        getIt<JwtAuthenticationService>();
+                    await service.getCurrentUser();
+                  },
+                  child: const Text('Check'))
             ],
           ),
         ),
